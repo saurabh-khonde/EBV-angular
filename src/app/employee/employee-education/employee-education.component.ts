fee7159c6fb86c8579f4d8a178ca684f83faa791
rahulmchavan <rahulchavhanm@gmail.com>
2020-07-14 18:55:40 +0530

    educational detail push by rahul on 14-7-2020 7-pm

--- src/app/employee/employee-education/employee-education.component.ts
+++ src/app/employee/employee-education/employee-education.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, NgForm, FormGroup, FormControl } from '@angular/forms';
+import { EmpEducationServiceService } from './emp-education-service.service';
+
+
 
 @Component({
   selector: 'app-employee-education',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-education.component.scss']
 })
 export class EmployeeEducationComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit(): void {
+    throw new Error("Method not implemented.");
   }
 
+  constructor(private service:EmpEducationServiceService) { }
+  title = 'EmpVerify';
+   
+  
+  empEducationForm = new FormGroup({  
+    'university' : new FormControl(null, Validators.required),  
+    'Stream' : new FormControl(null, Validators.required),
+    'CollegeName' : new FormControl(null, Validators.required), 
+    'YearOfPassing' : new FormControl(null, Validators.required),  
+    'Type':new FormControl(null, Validators.required),
+    'Qualification':new FormControl(null, Validators.required), 
+    'IsYearGap':new FormControl(null, Validators.required),
+    'Percentage' : new FormControl(null, Validators.required), 
+    'NoOfYearGap' : new FormControl(null, Validators.required),
+    'IsAccepted':new FormControl(null)  
+} ) 
+
+message:any
+// Executed When Form Is Submitted  
+onFormSubmit(form:NgForm)  
+{  
+  console.log(form);  
+
+  this.service.EmpEducationService(form).subscribe(
+    data => this.message=data,
+    error => this.message=error
+  )
+
+  console.log(this.message)
+}  
 }
