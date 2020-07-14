dd1925fb6a2f4d9afa79013ed2b5e433e693df87
swapnil idhate <swapnilidhate916@gmail.com>
2020-07-14 13:07:12 +0530

    family details component

--- src/app/employee/employee-family/employee-family.component.ts
+++ src/app/employee/employee-family/employee-family.component.ts
@@ -8,30 +8,26 @@ import { FormBuilder, Validators, NgForm, FormGroup, FormControl } from '@angula
 })
 export class EmployeeFamilyComponent implements OnInit {
 
-
-
-
   constructor() { 
-
-
   }
-
+ 
   ngOnInit(): void {
-
-    
   }
   regiForm = new FormGroup({  
-    'FamilyDetailsID' : new FormControl(null, Validators.required),  
     'FirstName' : new FormControl(null, Validators.required),  
     'LastName' : new FormControl(null, Validators.required),
     'DateOfBirth': new FormControl(null, Validators.required),
     'Occupation': new FormControl(null, Validators.required),
     'Relation':new FormControl(null, Validators.required),
     'Email':new FormControl(null, Validators.compose([Validators.required,Validators.email])),  
-    'MobileNumber':new FormControl(null, Validators.compose([Validators.required,Validators.pattern(/^[6-9]\d{9}$/), Validators.minLength(10), Validators.maxLength(10)])),
-    'EmployeeID1' : new FormControl(null, Validators.required),  
-    //'IsAccepted':new FormControl(null)  
+    'MobileNumber':new FormControl(null, Validators.compose([Validators.required,Validators.pattern(/^[6-9]\d{9}$/), Validators.minLength(10), Validators.maxLength(10)]))
+
 } ) 
+
+onFormSubmit(form:NgForm)  
+{  
+  console.log(form);  
+} 
 }
 
 
