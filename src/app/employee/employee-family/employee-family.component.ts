c400e0e944665b3cd7bd905ff32f415ca2e57d25
swapnil idhate <swapnilidhate916@gmail.com>
2020-07-14 12:35:04 +0530

    Family component

--- src/app/employee/employee-family/employee-family.component.ts
+++ src/app/employee/employee-family/employee-family.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, NgForm, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-family',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeFamilyComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+
+  constructor() { 
+
+
   }
 
+  ngOnInit(): void {
+
+    
+  }
+  regiForm = new FormGroup({  
+    'FamilyDetailsID' : new FormControl(null, Validators.required),  
+    'FirstName' : new FormControl(null, Validators.required),  
+    'LastName' : new FormControl(null, Validators.required),
+    'DateOfBirth': new FormControl(null, Validators.required),
+    'Occupation': new FormControl(null, Validators.required),
+    'Relation':new FormControl(null, Validators.required),
+    'Email':new FormControl(null, Validators.compose([Validators.required,Validators.email])),  
+    'MobileNumber':new FormControl(null, Validators.compose([Validators.required,Validators.pattern(/^[6-9]\d{9}$/), Validators.minLength(10), Validators.maxLength(10)])),
+    'EmployeeID1' : new FormControl(null, Validators.required),  
+    //'IsAccepted':new FormControl(null)  
+} ) 
 }
+
+
+
+
+
