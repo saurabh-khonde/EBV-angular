10495916caa2548ceb330319c5b5323d39b4acef
yogesh2527 <57997251+yogesh2527@users.noreply.github.com>
2020-07-15 13:51:54 +0530

    Update emp-employment-service.service.ts

--- src/app/employee/employee-employment/emp-employment-service.service.ts
+++ src/app/employee/employee-employment/emp-employment-service.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpEmploymentServiceService {
 
-  baseUrl = "localhost://8080/api/employeeDetauls/"; 
 
-  constructor(private http : HttpClient) { }
 
-  public saveEmployeeDetails(data)
-  {
-    return this.http.post(this.baseUrl,data);
-  }
+  constructor() { }
+
+
 }
